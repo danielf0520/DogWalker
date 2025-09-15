2809e70ec3c22c5e909ccf63bf879f156d01fef3
DanielFeng <danielf6635@nycstudents.net>
2025-09-15 13:20:04 -0400

    typed intro

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        public class DogWalkCompany
+        /**
+         * Returns the number of dogs, always greater than 0, that are available
+         * for a walk during the time specified by hour
+         * Precondition: 0 <= hour <= 23
+         */
+        public int numAvailableDogs(int hour)
+        {}
+        /**
+         * decreases, by numberDogsWalked, the number of dogs available for a walk
+         * during the time specified by hour
+         * Precondition: 0 <= hour = 23
+         * numberDogsWalked > 0
+         */
+        public void updateDogs(int hour, int numberDogsWalked)
+        {}
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
 }
